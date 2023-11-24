404fcff23ff060632060cf2593582ff3170371d2
Ali111q <93655964+Ali111q@users.noreply.github.com>
2023-11-24 08:28:37 +0300

    Update order.js

--- controller/order.js
+++ controller/order.js
@@ -1,6 +1,13 @@
 const { Order } = require("../model/products");
 
 async function getOrders(req, res){
-    const orders = Order.findAll();
+    const orders = await Order.findAll();
     // to do: return data
+}
+async function deleteOrder(req, res){
+    await Order.delete({
+        where:{
+            id: req.body.id
+        }
+    })
 }
