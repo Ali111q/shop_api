6cbd3cb8eabb9fe55c2e75fafefe0eeb3a5f6378
Ali111q <93655964+Ali111q@users.noreply.github.com>
2023-11-24 13:51:09 +0300

    Update driver.js

--- routes/driver.js
+++ routes/driver.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const {acceptOrder, getOrderById, getOrders, deliveredOrders} = require("..controller/driver");
+
+router.post("/accept", acceptOrder);
+router.get("/orders", getOrders);
+router.post("/done", deliveredOrders);
+router.get("/order", getOrderById)
