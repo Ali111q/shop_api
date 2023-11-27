9696586078c9b31c1a6fb5409b21cb83f387adba
Ali111q <93655964+Ali111q@users.noreply.github.com>
2023-11-27 15:30:57 -0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('shop', 'root', '', {
+const sequelize = new Sequelize('shop', 'root', 'Us.1159800', {
   host: 'localhost',
   dialect: 'mysql', // Change to your database dialect (e.g., postgres, sqlite)
 });
